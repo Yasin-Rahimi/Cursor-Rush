80a026090321c03f4876448818848034d61a0ed5
Yasin Rahimi <rahimiyasin58@gmail.com>
2025-12-23 00:19:29 +0330

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Carsor-Rush
-A fast-paced mouse-avoidance game built with React. Dodge randomly spawned circles and survive as long as you can.ل
--- src/components/GuessTheNameGame/GNGMain.jsx
+++
@@ -1,9 +0,0 @@
-import GNGContainer from "./GNGContainer";
-
-export default function GNGMain() {
-    return (
-        <>
-            <GNGContainer />
-        </>
-    )
-}
